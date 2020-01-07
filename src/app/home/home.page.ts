b399d11756f4e96405d0f96e53b242d4729ce550
jbdioli <jdeoworld@gmail.com>
2020-01-07 18:03:52 +0100

    add

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  videoBuffer: File;
+
   constructor() {}
+
+
+  onFileChosen(videoFile: any) {
+
+    this.videoBuffer = videoFile.target.files[0] as File;
+    console.log('Video Buffer : ', this.videoBuffer);
+    const type = this.videoBuffer.type;
+    console.log('Type : ', type);
+
+    const fileReader = new FileReader();
+
+
+    fileReader.onload = (ev) => {
+      // const blob = new Blob([ev.target.result], {type});
+      const blob = new Blob([fileReader.result], {type});
+      console.log('Blob :', blob);
+
+      const url = (URL || webkitURL).createObjectURL(blob);
+      const video = document.createElement('video');  // create video element
+      console.log('video document :', video);
+
+      video.preload = 'metadata';                     // preload setting
+      video.addEventListener('loadedmetadata', () => {
+        console.log('duration :', video.duration);
+        console.log('height : ', video.height);
+      });
+
+    };
+
+    fileReader.readAsArrayBuffer(this.videoBuffer);
+
+  }
 
 }
