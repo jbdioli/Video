b4f2330a9a082a6f279b449a04aeafd601a20f74
jbdioli <jdeoworld@gmail.com>
2020-01-08 12:21:38 +0100

    add

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+export interface VideoDetail {
+  name?: string;
+  type?: string;
+  duration?: number;
+  size?: number;
+  videoHeight?: number;
+  videoWidth?: number;
+  file?: File;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,24 +18,34 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   videoBuffer: File;
+  videoDetail: VideoDetail = {};
 
   constructor() {}
 
 
-  onFileChosen(videoFile: any) {
+  onFileChosen(ev: Event) {
+    console.log('VideoFile : ', ev);
 
-    this.videoBuffer = videoFile.target.files[0] as File;
+    const files: FileList = (ev.target as HTMLInputElement).files;
+
+    this.videoBuffer = files[0];
     console.log('Video Buffer : ', this.videoBuffer);
+
     const type = this.videoBuffer.type;
     console.log('Type : ', type);
 
     const fileReader = new FileReader();
 
+    this.videoDetail.name = this.videoBuffer.name;
+    this.videoDetail.type = this.videoBuffer.type;
+    this.videoDetail.size = this.videoBuffer.size;
+    this.videoDetail.file = this.videoBuffer;
 
-    fileReader.onload = (ev) => {
-      // const blob = new Blob([ev.target.result], {type});
+
+    fileReader.onload = () => {
       const blob = new Blob([fileReader.result], {type});
       console.log('Blob :', blob);
+      // this.videoDetail.file = blob;
 
       const url = (URL || webkitURL).createObjectURL(blob);
       const video = document.createElement('video');  // create video element
@@ -33,9 +53,17 @@ export class HomePage {
 
       video.preload = 'metadata';                     // preload setting
       video.addEventListener('loadedmetadata', () => {
+        this.videoDetail.duration = video.duration;
+
+        this.videoDetail.videoHeight = video.videoHeight;
+        this.videoDetail.videoWidth = video.videoWidth;
         console.log('duration :', video.duration);
-        console.log('height : ', video.height);
+        console.log('height : ', video.videoHeight);
+        console.log('Width : ', video.videoWidth);
       });
+
+
+      video.src = url; // start video load
 
     };
 
